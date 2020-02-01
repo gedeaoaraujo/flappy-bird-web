b9dc5d326deb69d9e76bc1206b953f28725006c6
Gedeao <gedeao.araujo@outlook.com.br>
2020-02-01 00:17:26 -0300

    Refactoring packages

--- js/flappy.js
+++ js/flappy.js
@@ -69,7 +69,7 @@ function Passaro(alturaJogo) {
     let voando = false
 
     this.elemento = novoElemento('img', 'passaro')
-    this.elemento.src = 'passaro.png'
+    this.elemento.src = 'img/passaro.png'
 
     this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
     this.setY = y => this.elemento.style.bottom = `${y}px`
